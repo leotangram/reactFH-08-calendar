b6710604743f6233d594532c3e3d8cbc8a783d66
Leonardo Omaña <leotangram@gmail.com>
2020-09-23 10:05:42 -0500

    Configuraciones adicionales al calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import moment from 'moment'
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import Navbar from '../ui/Navbar'
+import 'moment/locale/es'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import { messages } from '../../helpers/calendar-messages-es'
+import Navbar from '../ui/Navbar'
 
 const localizer = momentLocalizer(moment)
 
@@ -11,11 +13,34 @@ const events = [
     title: 'Cumpleaños del jefe',
     start: moment().toDate(),
     end: moment().add(2, 'hours').toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#fafafa',
+    notes: 'Comprar el pastel'
   }
 ]
 
 const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log(
+      'event: ',
+      event,
+      'start: ',
+      start,
+      'end: ',
+      end,
+      'isSelected: ',
+      isSelected
+    )
+    const style = {
+      backgroundColor: '#367cf7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display: 'block',
+      color: '#ffffff'
+    }
+
+    return { style }
+  }
+
   return (
     <div className="calendar-screen">
       <Navbar />
@@ -24,6 +49,8 @@ const CalendarScreen = () => {
         events={events}
         startAccesor="start"
         endAccesor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
       />
     </div>
   )
