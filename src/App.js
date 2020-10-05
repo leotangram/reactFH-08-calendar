b4fa9952d52bd18220d489fb27b8f7c061b7768d
Leonardo Omaña <leotangram@gmail.com>
2020-10-05 15:03:28 -0500

    Instalación y configuración de Redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 import AppRouter from './router/AppRouter'
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       <AppRouter />
-    </div>
+    </Provider>
   )
 }
 
