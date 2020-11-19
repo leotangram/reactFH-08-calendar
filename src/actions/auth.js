13ec1dacfafdb0625bb6fe7ce5b38896539f74ae
Leonardo Omaña <leotangram@gmail.com>
2020-11-19 15:37:48 -0500

    Realizar la petición http

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,24 @@
+import { fetchNoToken } from '../helpers/fetch'
+import { types } from '../types/types'
+
 export const startLogin = (email, password) => {
-  return async () => {
-    console.log(email, password)
+  return async dispatch => {
+    try {
+      const response = await fetchNoToken('auth', { email, password }, 'POST')
+      const { name, ok, token, uid } = await response.json()
+
+      if (ok) {
+        localStorage.setItem('token', token)
+        localStorage.setItem('token-init-date', new Date().getTime())
+        dispatch(login({ uid, name }))
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
+
+const login = user => ({
+  type: types.authLogin,
+  payload: user
+})
